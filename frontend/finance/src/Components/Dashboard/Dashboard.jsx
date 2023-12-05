edcef1785e7cfd155770653e8c5fd674973c691a
KiRaN-110 <116154751+KiRaN-110@users.noreply.github.com>
2023-12-05 16:38:50 +0530

    Update Dashboard.jsx

--- frontend/finance/src/Components/Dashboard/Dashboard.jsx
+++ frontend/finance/src/Components/Dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { LoginSignUp } from '../LoginSignUp/LoginSignUp';
 
 export const Dashboard = () => {
   const { user_id } = useParams();
   const [userInfo, setUserInfo] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -23,8 +26,18 @@ export const Dashboard = () => {
     fetchUserInfo();
   }, [user_id]);
 
+  const handleLogout = () => {
+    // Perform logout functionality here
+    // For now, you can simply display a message
+    navigate('../LoginSignUp/LoginSignUp');
+    console.log('Logout clicked');
+  };
+
   return (
     <div>
+      <div style={{ textAlign: 'right', padding: '10px' }}>
+        <button onClick={handleLogout}>Log Out</button>
+      </div>
       <h1>Welcome to the Dashboard</h1>
       {userInfo && <p>Hello, {userInfo.name}!</p>}
     </div>
